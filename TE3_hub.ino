2978258fb4ea5f2fdf7a5cc9ae78b1e14806d813
phorton1 <available@upon.request>
2024-05-19 11:34:06 +0000

    restructure and cleanup

--- TE3_hub.ino
+++ TE3_hub.ino
@@ -14,25 +14,53 @@
 #include <Wire.h>
 #include <myDebug.h>
 #include <teSGTL5000.h>
+#include <teMidi.h>
 #include "src/midiHost.h"
 
 
-#define USB_SERIAL_PORT		Serial
-#define MIDI_SERIAL_PORT	Serial1
-#define DEBUG_SERIAL_PORT	Serial3
-#define USE_DEBUG_PORT		USB_SERIAL_PORT
+#define USB_SERIAL_PORT			Serial
+#define MIDI_SERIAL_PORT		Serial1
+#define ALT_SERIAL_PORT			0	// Serial3
+
+
+#define DEBUG_TO_USB_SERIAL		0
+#define DEBUG_TO_MIDI_SERIAL	1
+#define DEBUG_TO_ALT_SERIAL		2
+
+#define HOW_DEBUG_OUTPUT		DEBUG_TO_MIDI_SERIAL
+
+
+
+#define USE_DEBUG_PORT			USB_SERIAL_PORT
+#define USE_SERIAL_MIDI_PORT	MIDI_SERIAL_PORT
+
 
 #define SPOOF_FTP  0
+	// requires WITH_MIDI_HOST in midiHost.h
+	// however, initial tests reveal that an active midiHost
+	// causes severely noticable buzz on the teensy audio shield,
+	// which I *doubt* is fixable. Might be important enough to
+	// try various methods of isolating the SGTL5000, but more
+	// important are basic audio tests with the guitar, USB Audio
+	// and the Looper, so this issue is on hold.
+	//
+	// Current plan is to go back to a teensy 4.1 for TE3, and
+	// try to ram the HOST via Serial Midi to the TE3_hub fast,
+	// and quiet, enough for usage.
 
 
 
-#define WITH_SERIAL3	0
-	// must set this to use DEBUG_SERIAL_PORT for debugging
 
 #define TRIGGER_PIN		0		// 13
 	// set this to a pin to trigger logic analyzer
 #define FLASH_PIN		13
 	// set this to a pin to flash during loop
+
+
+
+//-------------------------------------------
+// audio system setup
+//-------------------------------------------
 
 #define TONE_SWEEP_TEST	0
 	// set to 1 to do simple autdio tone out test
@@ -42,8 +70,7 @@
 	// allows to monitor I2S_IN and mix it with USB_IN to I2S_OUT
 
 
-
-
+SGTL5000 sgtl5000;
 
 
 #if TONE_SWEEP_TEST
@@ -57,6 +84,17 @@
 	int t_lox = 10;
 	int t_hix = 4000;
 	float t_timex = 10;
+
+	void toneSweepTest()
+	{
+		if (!tone_sweep.play(t_ampx,t_lox,t_hix,t_timex))
+		{
+			my_error("tone_sweep.play() failed",0);
+			delay(10000);
+			return;
+		}
+		while (tone_sweep.isPlaying());
+	}
 
 #else
 
@@ -109,71 +147,13 @@
 		AudioConnection     c1(i2s_in, 0, i2s_out, 0);
 		AudioConnection     c2(i2s_in, 1, i2s_out, 1);
 
-	#endif
+	#endif	// !WITH_USB_AUDIO
+
+#endif	// !TONE_SWEEP_TEST
 
 
-#endif
-
-
-SGTL5000 sgtl5000;
-
-extern "C" {
-    extern void my_usb_init();          	// in usb_dev.c
-    extern void setFTPDescriptors();    	// _usbNames.c
-	extern const char *getUSBSerialNum();	// _usbNames.c
-}
-
-
-// #include "src/_usb.h"
-
-
-void setup()
+void audioExperiments()
 {
-	#if SPOOF_FTP
-		setFTPDescriptors();
-    #endif
-
-	my_usb_init();
-    delay(1000);
-
-	Serial.begin(115200);
-	delay(100);
-	Serial1.begin(115200);
-	delay(100);
-	#if WITH_SERIAL3
-		Serial3.begin(115200);
-		delay(100);
-	#endif
-
-	setColorString(COLOR_CONST_DEFAULT, "\033[94m");	// bright blue
-        // TE3_hub's normal display color is bright blue
-        // TE3's normal (default) display color is green
-        // Looper's normal display color, is cyan, I think
-	dbgSerial = &USE_DEBUG_PORT;
-
-	delay(250);
-	display(0,"TE3_hub.ino setup() started",0);
-
-	pinMode(FLASH_PIN,OUTPUT);
-
-	#if TRIGGER_PIN
-		pinMode(TRIGGER_PIN,OUTPUT);
-		digitalWrite(TRIGGER_PIN,0);
-	#endif
-
-	AudioMemory(100);
-	delay(250);
-
-	#if TRIGGER_PIN
-		digitalWrite(TRIGGER_PIN,1);
-	#endif
-
-	// Note: the artisan Pifi seems to put out about the same signal
-	// via either the HP jack or the line out plugs.
-
-	sgtl5000.enable();
-	sgtl5000.setInput(SGTL_INPUT_LINEIN);		// line_in == 0
-
 	// Chain from left to right
 
 	#if 0
@@ -218,12 +198,6 @@ void setup()
 			// to be called AFTER setHeadphoneVolume(>=0).
 	#endif
 	
-	#if 1
-		sgtl5000.setDefaultGains();
-	#elif 0
-		gtl5000.dispatchCC(SGTL_CC_SET_DEFAULT_GAINS,0);
-	#endif
-
 	// try various DAP effects
 
 	#if 0
@@ -271,8 +245,115 @@ void setup()
 		sgtl5000.setMuteHeadphone(0);
 	#endif
 
-	
-	sgtl5000.dumpCCValues("after my init");
+}
+
+
+
+
+
+//=================================================
+// setup()
+//=================================================
+
+extern "C" {
+    extern void my_usb_init();          	// in usb_dev.c
+    extern void setFTPDescriptors();    	// _usbNames.c
+	extern const char *getUSBSerialNum();	// _usbNames.c
+}
+
+
+
+void setup()
+{
+	#if FLASH_PIN
+		pinMode(FLASH_PIN,OUTPUT);
+		digitalWrite(FLASH_PIN,1);
+	#endif
+
+	//-----------------------------------------
+	// initialize hardware serial ports
+	//-----------------------------------------
+	// most important being the MIDI_SERIAL_PORT
+
+	setColorString(COLOR_CONST_DEFAULT, "\033[94m");	// bright blue
+        // TE3_hub's normal display color is bright blue
+        // TE3's normal (default) display color is green
+        // Looper's normal display color, is cyan, I think
+
+	MIDI_SERIAL_PORT.begin(115200);		// Serial1
+	delay(100);
+	#if HOW_DEBUG_OUTPUT == DEBUG_TO_MIDI_SERIAL
+		dbgSerial = &MIDI_SERIAL_PORT;
+		display(0,"TE3_hub.ino setup() started on MIDI_SERIAL_PORT",0);
+	#endif
+
+	#if ALT_SERIAL_PORT
+		ALT_SERIAL_PORT.begin(115200);	// Serial3
+		delay(100);
+		#if HOW_DEBUG_OUTPUT == DEBUG_TO_ALT_SERIAL
+			dbgSerial = &ALT_SERIAL_PORT;
+			display(0,"TE3_hub.ino setup() started on ALT_SERIAL_PORT",0);
+		#endif
+	#endif
+
+
+	//-------------
+	// init usb
+	//-------------
+
+	#if SPOOF_FTP
+		setFTPDescriptors();
+    #endif
+
+	my_usb_init();
+    delay(500);
+
+	#if FLASH_PIN
+		digitalWrite(FLASH_PIN,0);
+	#endif
+
+	//---------------------------------
+	// initialize USB_SERIAL_PORT
+	//---------------------------------
+
+	USB_SERIAL_PORT.begin(115200);		// Serial.begin()
+	delay(100);
+	#if HOW_DEBUG_OUTPUT == DEBUG_TO_ALT_SERIAL
+		display(0,"TE3_hub.ino setup() started on USB_SERIAL_PORT",0);
+	#endif
+
+
+	//-----------------------------------
+	// initialize the audio system
+	//-----------------------------------
+
+	delay(250);
+	display(0,"initializing audio system",0);
+
+	#if TRIGGER_PIN
+		pinMode(TRIGGER_PIN,OUTPUT);
+		digitalWrite(TRIGGER_PIN,0);
+	#endif
+
+	AudioMemory(100);
+	delay(250);
+
+	#if TRIGGER_PIN
+		digitalWrite(TRIGGER_PIN,1);
+	#endif
+
+	// Note: the artisan Pifi seems to put out about the same signal
+	// via either the HP jack or the line out plugs.
+
+	sgtl5000.enable();
+	sgtl5000.setInput(SGTL_INPUT_LINEIN);		// line_in == 0
+	// audioExperiments();
+
+	#if 1
+		sgtl5000.setDefaultGains();
+		sgtl5000.dumpCCValues("after setDefaultGains()");
+	#endif
+
 
 	#if WITH_USB_AUDIO
 		#if WITH_MIXERS
@@ -282,12 +363,22 @@ void setup()
 			mixer_r.gain(1, 1.0);
 		#endif
 	#endif
-	
+
+
+	//--------------------------------------
+	// midiHost
+	//--------------------------------------
 
 	#if WITH_MIDI_HOST
 		// defined in midiHost.h
 		display(0,"initilizing midiHost",0);
 		midi_host.init();
+	#endif
+
+	// setup finished
+
+	#if FLASH_PIN
+		digitalWrite(FLASH_PIN,1);
 	#endif
 
 	display(0,"TE3_hub.ino setup() finished",0);
@@ -296,45 +387,49 @@ void setup()
 
 
 
+
+//=============================================
+// loop()
+//=============================================
+
 void loop()
 {
-    static bool flash_on = 0;
-    static uint32_t flash_last = 0;
-    uint32_t now = millis();
-    if (now > flash_last + 1000)
-    {
-        flash_last = now;
-        flash_on = !flash_on;
-        digitalWrite(FLASH_PIN,flash_on);
+	#if FLASH_PIN
+		static bool flash_on = 0;
+		static uint32_t flash_last = 0;
+		uint32_t now = millis();
+		if (now > flash_last + 1000)
+		{
+			flash_last = now;
+			flash_on = !flash_on;
+			digitalWrite(FLASH_PIN,flash_on);
 
-		#if 0
-			static int counter = 0;
-			display(0,"loop(%d)",counter++);
-			// Serial1.print("counter=");
-			// Serial1.println(counter);
+			#if 0
+				static int counter = 0;
+				display(0,"loop(%d)",counter++);
 
-			#if 1
-				// send an arbitray midi message
+				#if 0
+					// send an arbitray midi message
+					uint32_t val = counter % 128;
+					val <<= 24;
+					uint32_t msg = 0x0022b70b | val;
+					usb_midi_write_packed(msg);
+				#endif
 
-				uint32_t val = counter % 128;
-				val <<= 24;
-				uint32_t msg = 0x0022b70b | val;
-				usb_midi_write_packed(msg);
 			#endif
-
-		#endif
-    }
+	    }
+	#endif // FLASH_PIN
 
 
 	#if TONE_SWEEP_TEST
-		if (!tone_sweep.play(t_ampx,t_lox,t_hix,t_timex))
-		{
-			my_error("tone_sweep.play() failed",0);
-			delay(10000);
-			return;
-		}
-		while (tone_sweep.isPlaying());
+		toneSweepTest();
 	#endif
+
+
+	//-----------------------------
+	// set SGTL5000 from USB
+	//-----------------------------
+	// interesting possibility to set levels from iPad
 
 	#if 0	// vestigal code to set SGTL5000 volume from USB device
 		float vol = usb_in.volume();  		// read PC volume setting
@@ -353,21 +448,6 @@ void loop()
 	// 	if (midi_host.read())
 	//		display(0,"HOST(%02x,%02x,%02x)",midi_host.getType(), midi_host.getData1(), midi_host.getData2());
 
-
-	//------------------------------------------------------
-	// a lack of any delay here may cause problems
-	//------------------------------------------------------
-	// for USB and rPi Audio processing?
-
-	#if 0
-		delay(50);
-	#endif
-
-	#if 0
-		// handle SGTL5000 eq automation
-		sgtl5000.loop();
-	#endif
-
 	#if WITH_MIDI_HOST && SPOOF_FTP
 		uint32_t msg32 = usb_midi_read_message();  // read from device
 		if (msg32)
@@ -376,7 +456,95 @@ void loop()
 			midi_host.write_packed(msg32);
 		}
 	#endif
+
+
+	//------------------------------------------------------
+	// Normal Processing
+	//------------------------------------------------------
+
+	#if 0
+		delay(50);
+	#endif
+
+	handleSerialMidi();
+
+	#if 0
+		// handle SGTL5000 eq automation
+		sgtl5000.loop();
+	#endif
+
 }
+
+
+//==============================================================
+// Serial MIDI handler
+//==============================================================
+
+#define dbg_sm  0
+
+
+#define RAW_SGTL5000_CABLE 	(0x0E << 4)
+#define knownCable(byte)	(((byte) & RAW_SGTL5000_CABLE) == RAW_SGTL5000_CABLE)
+#define SGTL5000_CC			(RAW_SGTL5000_CABLE | MIDI_TYPE_CC)
+
+
+void handleSerialMidi()
+{
+	static uint32_t msg32 = 0;
+	static uint8_t *buf = (uint8_t *)&msg32;
+	static uint8_t len = 0;
+
+	// code expects a leading SERIAL_MIDI byte that
+	// has a known cable. The redundant message 'type'
+	// in the leading byte is not checked.
+	//
+	// If an unknown cable is detected, it will report an
+	// error and skip the byte in an attempt to try to
+	// synchronize to the MIDI stream.  Otherwise it buffers
+	// four bytes for handling.
+
+	while (MIDI_SERIAL_PORT.available())
+	{
+		uint8_t byte = MIDI_SERIAL_PORT.read();
+
+		if (len == 0)
+		{
+			if (knownCable(byte))
+			{
+				buf[len++] = byte;
+			}
+			else
+			{
+				my_error("TE3_hub: unexpected cable in MIDI byte0(0x%02x)",byte);
+			}
+		}
+		else
+		{
+			buf[len++] = byte;
+			if (len == 4)
+			{
+				display(dbg_sm,"<-- %08x",msg32);
+
+				msgUnion msg(msg32);
+
+				if (msg.cable() == SGTL5000_CABLE &&
+					msg.channel() == SGTL5000_CHANNEL &&
+					msg.type() == MIDI_TYPE_CC)
+				{
+					sgtl5000.dispatchCC(msg.param1(),msg.param2());
+				}
+				else
+				{
+					my_error("TE3_hub: unexpeced serial midi(0x%08x)",msg32);
+				}
+
+				len = 0;
+			}
+		}
+	}
+
+}
+
 
 
 
